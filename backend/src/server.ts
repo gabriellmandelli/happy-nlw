d66263fc8db61d74a0f288cec55f354f7a39297a
gabriellmandelli <gabriel-mandelli@hotmail.com>
2020-10-13 17:02:48 -0300

    backend database connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,11 @@
 import express from 'express'
 
+import './database/connection'
+
 const app = express();
+
+app.get('/users', (request, response) => {
+    return response.json({message : 'Teste'})
+})
 
 app.listen(3333);
