b8b4055a44bfe088408ac2f8e608ccf7c8967733
gabriellmandelli <gabriel-mandelli@hotmail.com>
2020-10-15 13:37:54 -0300

    backend FindById and upload orphanato images

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express'
 import { getRepository } from 'typeorm'
+import * as Yup from 'yup'
 import Orphanage from '../models/Orphanage'
+import OrphanageView from '../views/OrphanagesView'
 
 export default {
     async create(request : Request, response : Response) {
@@ -15,8 +17,14 @@ export default {
         } = request.body
     
         const orphanagesRespository = getRepository(Orphanage)
-    
-        const orphanage = orphanagesRespository.create({
+
+        const requestImages = request.files as Express.Multer.File[]
+
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        })
+
+        const data = {
             name,
             latitude,
             longitude,
@@ -24,7 +32,28 @@ export default {
             instructions,
             opening_hours,
             open_on_weekends,
+        }
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(
+                Yup.object().shape({
+                    path: Yup.string().required()
+                })
+            )
         })
+
+        await schema.validate(data, {
+            abortEarly: false,
+        })
+    
+        const orphanage = orphanagesRespository.create(data)
     
         await orphanagesRespository.save(orphanage)
     
@@ -34,8 +63,23 @@ export default {
     async findAll(request : Request, response : Response){
         const orphanagesRespository = getRepository(Orphanage)
 
-        const orphanagesList = await orphanagesRespository.find()
+        const orphanagesList = await orphanagesRespository.find({
+            relations: ['images']
+        })
 
-        return response.json(orphanagesList)
+        return response.json(OrphanageView.renderMany(orphanagesList))
+    },
+
+    async findById(request : Request, response : Response){
+
+        const { id } = request.params
+
+        const orphanagesRespository = getRepository(Orphanage)
+
+        const orphanage = await orphanagesRespository.findOneOrFail(id, {
+            relations: ['images']
+        })
+        
+        return response.json(OrphanageView.render(orphanage))
     }
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express'
+import multer from 'multer'
+
+import uploadConfig from './config/upload'
 import OrphanagesController from './controllers/OrphanagesController'
 
 const routes = Router()
+const upload = multer(uploadConfig)
 
-routes.post('/orphanages', OrphanagesController.create)
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create)
 routes.get('/orphanages', OrphanagesController.findAll)
+routes.get('/orphanages/:id', OrphanagesController.findById)
 
 export default routes
